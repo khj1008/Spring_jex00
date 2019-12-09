1597c6f6f5fbe86cb70d1e3d25e2658bcff3b2a2
kimhyju <kimhyju703@naver.com>
2019-12-10 02:46:27 +0900

    initial commit

---
+++ src/main/java/org/zerock/config/WebConfig.java
@@ -0,0 +1,25 @@
+package org.zerock.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	//root-context.xml을 대신하는 클래스 지정
+	protected Class<?>[] getRootConfigClasses() {
+		
+		return new Class[] {RootConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
